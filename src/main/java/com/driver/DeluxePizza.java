567a1bdd4f9851a69ac80fc03c1355650d167314
Aswinkumar20 <aswinkumar11999@gmail.com>
2022-10-09 11:19:35 +0530

    added deluxe

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -1,9 +1,11 @@
 package com.driver;
 
 public class DeluxePizza extends Pizza {
-
+//    int cheeseCount = 0;
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
         // your code goes here
+        cheeseCount =1;
+        toppingsCount=1;
     }
 }
