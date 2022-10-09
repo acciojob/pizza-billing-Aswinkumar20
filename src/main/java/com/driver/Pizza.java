96be367d77e25acad91ba5284d07ba8d62352d07
Aswinkumar20 <aswinkumar11999@gmail.com>
2022-10-09 10:43:28 +0530

    added

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,11 +5,30 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
-
+    int cheeseCount;
+    int toppingsCount;
+    int paperBagCount;
     public Pizza(Boolean isVeg){
+        this.cheeseCount = 0;
+        this.toppingsCount =0;
+        this.paperBagCount = 0;
         this.isVeg = isVeg;
+        this.bill ="";
+
+
         // your code goes here
+        if(isVeg){
+            price = 300;
+            bill += "Base Price Of The Pizza: 300\n";
+//            System.out.println("Base Price Of The Pizza: "+ price);
+        }else{
+            price = 400;
+            bill += "Base Price Of The Pizza: 400\n";
+//            System.out.println("Base Price Of The Pizza: "+ price);
+
+        }
     }
+
 
     public int getPrice(){
         return this.price;
@@ -17,18 +36,47 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        int extraCheese = 80;
+        if(cheeseCount == 0){
+//            System.out.println("Extra Cheese Added: "+ extraCheese);
+            price += 80;
+            bill += "Extra Cheese Added: 80\n";
+        }
+        cheeseCount++;
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(toppingsCount ==0 ){
+            if(isVeg){
+                price += 70;
+                bill += "Extra Toppings Added: 70\n";
+//                System.out.println("Extra Toppings Added: 70");
+            }else {
+                price += 120;
+                bill += "Extra Toppings Added: 120\n";
+//                System.out.println("Extra Toppings Added: 120");
+            }
+        }
+
+        toppingsCount++;
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(paperBagCount == 0){
+            price += 20;
+            bill += "Paperbag Added: 20\n";
+//        System.out.println("Paperbag Added: 20");
+        }
+
+
+        paperBagCount++;
     }
 
     public String getBill(){
         // your code goes here
+        bill += "Total Price: " +price +"\n";
         return this.bill;
     }
 }
