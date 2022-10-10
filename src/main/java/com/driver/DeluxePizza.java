0aa6b45f9f36514c38f726490ad595dd6d64af55
Aswinkumar20 <aswinkumar11999@gmail.com>
2022-10-10 08:11:33 +0530

    addedd

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -8,12 +8,4 @@ public class DeluxePizza extends Pizza {
         super.addExtraCheese();
         super.addExtraToppings();
     }
-
-    public void addExtraCheese(){
-
-    }
-
-    public void addExtraToppings(){
-
-    }
 }
